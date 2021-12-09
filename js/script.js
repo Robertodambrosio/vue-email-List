9ee9d3e81c21344d0f907df35ba5c51940478775
Roberto D'Ambrosio <roberto8601@gmail.com>
2021-12-09 16:26:25 +0100

    es

---
+++ js/script.js
@@ -0,0 +1,17 @@
+const app = new Vue({
+    el: '#root',
+    data: {
+        email: []
+    },
+    methods: {
+
+    },
+    created() {
+        for (i = 0; i < 10; i++) {
+        axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+        .then ((response) => {
+            this.email.push(response.data.response)
+        })
+        }
+    }
+})
